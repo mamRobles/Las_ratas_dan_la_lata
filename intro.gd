2b38ab8d88f526ab6ce1998abefd625a871065dd
Helena <115306258+HelenaMJ@users.noreply.github.com>
2025-04-01 13:46:24 +0200

    Menú de selección de cantidad de jugadores e intro

---
+++ intro.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var label: Label = $PanelContainer/MarginContainer/VBoxContainer/Label
+
+func _ready():
+	label.text = "¡Van a jugar " + str(raiz.num_jugadores) + " ratas!"
+	
+	#Contenedor vertical
+	var contenedor = get_node("PanelContainer/MarginContainer/VBoxContainer")
+	
+	#Para cada jugador
+	for i in range(raiz.num_jugadores):
+		#Se crea un nodo Label
+		var nombre = Label.new() 
+		#Nombre del nodo
+		nombre.name = "NombreJugador" + str(i+1) 
+		#Texto de la Label
+		nombre.text = raiz.nombres_jugadores[i]
+		#Se añade el nodo al contenedor
+		contenedor.add_child(nombre) 
+		#Separación entre los hijos del contenedor
+		contenedor.add_theme_constant_override("separation", 50)
+		
+		#Atributo style de la Label
+		var style = StyleBoxFlat.new()
+		style.bg_color = Color8(0, 120, 80)  #Color de la caja
+		style.corner_radius_top_left = 10    #Redondear bordes
+		style.corner_radius_top_right = 10
+		style.corner_radius_bottom_left = 10
+		style.corner_radius_bottom_right = 10
+		#Se añade el atributo style 
+		nombre.add_theme_stylebox_override("normal", style)
+		
+		#Centrar el texto dentro de la Label
+		nombre.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		nombre.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		#Tamaño mínimo de la Label
+		nombre.custom_minimum_size = Vector2(150, 50)
---
+++ scj.gd
@@ -0,0 +1,75 @@
+extends Control
+
+#var num_jugadores:int = 0
+#var nombres_jugadores = []
+
+
+func _on_2PJ_pressed() -> void:
+	raiz.num_jugadores = 2
+	setNombresJugadores()
+
+
+func _on_3PJ_pressed() -> void:
+	raiz.num_jugadores = 3
+	setNombresJugadores()
+
+func _on_4PJ_pressed() -> void:
+	raiz.num_jugadores = 4
+	setNombresJugadores()
+	
+
+func setNombresJugadores() -> void:
+	#Contenedor con los nodos LineEdit
+	var contenedor = get_node("PanelContainer/MarginContainer/VBoxContainer/HBoxContainer2")
+	
+	#Limpiar la lista de nombres y el contenedor
+	get_node("PanelContainer/MarginContainer/VBoxContainer/Button").disabled = true
+	raiz.nombres_jugadores.clear()
+	if contenedor.get_child_count() > 0:  
+		for child in contenedor.get_children(): 
+			child.queue_free() 
+	
+	#Para cada jugador
+	for i in range(raiz.num_jugadores):
+		#Se crea un nodo LineEdit
+		var input_field = LineEdit.new() 
+		#Nombre del nodo
+		input_field.name = "NombreInput" + str(i+1) 
+		#Se añade el nodo al contenedor
+		contenedor.add_child(input_field) 
+		#Separación entre los hijos del contenedor
+		contenedor.add_theme_constant_override("separation", 20)
+		
+		#Atributo style del LineEdit
+		var style = StyleBoxFlat.new()
+		style.bg_color = Color8(0, 120, 80)  #Color de la caja
+		style.corner_radius_top_left = 10    #Redondear bordes
+		style.corner_radius_top_right = 10
+		style.corner_radius_bottom_left = 10
+		style.corner_radius_bottom_right = 10
+		#Se añade el atributo style (normal, seleccionado, cursor encima)
+		input_field.add_theme_stylebox_override("normal", style)
+		input_field.add_theme_stylebox_override("focus", style)
+		input_field.add_theme_stylebox_override("hover", style)
+		
+		#Texto en el recuadro antes de introducir un nombre
+		input_field.alignment = HORIZONTAL_ALIGNMENT_CENTER
+		input_field.placeholder_text = "Rata " + str(i+1)
+		#Tamaño mínimo de cada hijo 
+		input_field.custom_minimum_size = Vector2(150, 60)
+		#Expandir horizontalmente para ocupar todo el contenedor
+		input_field.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		
+		#Llama a la función _name_entered() cuando se introduce un nombre
+		input_field.text_submitted.connect(_name_entered)
+
+
+func _name_entered(nombre: String) -> void:
+	raiz.nombres_jugadores.append(nombre)
+	
+	if len(raiz.nombres_jugadores) == raiz.num_jugadores:
+		get_node("PanelContainer/MarginContainer/VBoxContainer/Button").disabled = false
+
+
+func _on_continuar_pressed() -> void:
+	get_tree().change_scene_to_file("res://intro.tscn")
