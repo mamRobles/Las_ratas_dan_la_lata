e432f886f3fa0bb0dfa4c099894b76f52c538e61
María del Mar <114167756+mamRobles@users.noreply.github.com>
2025-04-07 20:57:54 +0200

    Nodo jugador, con colisión, movimiento derecha/izquierda, salto (sin implementar la tecla de saltar), sprites animados de derecha, izquierda y parado. No tiene escondido/no escondido

---
+++ mini1_jugador.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+@onready var _animated_sprite = $AnimatedSprite2D
+@onready var accion =0 # sirve para saber si la última tecla pulsada es derecha o izquierda
+					   # y así definir la animación idle
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("UI_izquierda", "UI_derecha")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _process(_delta):
+	if Input.is_action_pressed("UI_derecha"):
+		accion=1
+		_animated_sprite.play("derecha")
+	else: if Input.is_action_pressed("UI_izquierda"):
+		accion=0
+		_animated_sprite.play("izquierda")
+	else:
+		if (accion):
+			_animated_sprite.play("parado_derecha")
+		else:
+			_animated_sprite.play("parado_izquierda")
