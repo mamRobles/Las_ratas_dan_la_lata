720835d1ae239b3ef16053ddb9ff34ff7bc5aabc
María del Mar <114167756+mamRobles@users.noreply.github.com>
2025-05-02 12:34:42 +0200

    Arreglado el botón ESC como pausa.

--- Sistema/Scripts/pausa.gd
+++ Sistema/Scripts/pausa.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,20 +7,29 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused==false:
+			pausar()
+		else:
+			despausar()
 
-
-func _on_boton_pausa_pressed() -> void:
+func pausar()->void:
 	$Boton_pausa.hide()
 	get_tree().paused =true
 	$Menu_Pausa.show()
+	
+func despausar()->void:
+	$Menu_Pausa.hide()
+	get_tree().paused=false
+	$Boton_pausa.show()
+	
+func _on_boton_pausa_pressed() -> void:
+	pausar()
 
 
 func _on_volver_juego_pressed() -> void:
-	$Boton_pausa.show()
-	$Menu_Pausa.hide()
-	get_tree().paused=false
-	pass # Replace with function body.
+	despausar()
+	
 	
 
 func _on_menu_principal_pressed() -> void:
