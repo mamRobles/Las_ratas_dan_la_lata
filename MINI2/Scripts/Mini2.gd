6bd3c179f5fc3604ae3fbc86caa50232613b6374
María del Mar <114167756+mamRobles@users.noreply.github.com>
2025-05-14 17:23:36 +0200

    Ahora la escena cambiante tiene un evento aleatorio que puede ser de varios tipos (puerta, atico, ventana, tumba) y que tiene una posibilidad aleatoria de mostrar o no a nosfegatu

---
+++ MINI2/Scripts/Mini2.gd
@@ -0,0 +1,20 @@
+extends Node2D
+signal hora_del_evento
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if($"Imagen-mini2".sin_evento):
+		$"Imagen-mini2".sin_evento=false
+		#random entre 3 y 6
+		var tiempo:int =(randi()%10)+6
+		await get_tree().create_timer(tiempo).timeout
+		hora_del_evento.emit()
+
+#qué hacer cuando nosfegatu aparezca
+func _on_imagenmini_2_nosfegatu_aparece() -> void:
+	pass # Replace with function body.
--- MINI2/Scripts/imagen_mini_2.gd
+++ MINI2/Scripts/imagen_mini_2.gd
@@ -1,11 +1,47 @@
 extends Control
+signal nosfegatu_aparece #señal que llega al escenario
+						# que dice que nosfegatu apareció
+@export var sin_evento:bool =true
 
+#si pasa demasiado tiempo sin aparecer nosfegatu, su probabilidad de
+#aparecer se vuelve 100%, eso es la hora de la bestia
+var hora_de_la_bestia :bool  =false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play("default")
 
 
+func _evento(evento:String)->void:
+	$AnimatedSprite2D.play("{n}".format({"n":evento}))
+	var gatu:bool = randi()%2
+	
+	if(gatu or hora_de_la_bestia):
+		#aparece nosfegatu
+		$AnimatedSprite2D.play("{n}_si".format({"n":evento}))
+		$nosfegatu.show()
+		nosfegatu_aparece.emit()
+	else:
+		#no aparece nosfegatu, reproducimos la animación sin él
+		$AnimatedSprite2D.play("{n}_no".format({"n":evento}))
+		#esperamos un poquito, medio segundo
+		await get_tree().create_timer(0.5).timeout
+		# volvemos a la animación por dfecto
+		$AnimatedSprite2D.play("default")
+		#le decimos al Node2d que puede poner otro temporizador para los eventos
+		sin_evento =true
+
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_hora_de_la_bestia_timeout() -> void:
+	hora_de_la_bestia=true
+
+#cuando el temporziador del evento se lanza, se empieza un nuevo evento
+func _on_node_2d_hora_del_evento() -> void:
+	var evento_array: Array[String] = [ "puerta", "tumba", "atico","ventana" ]
+	_evento(evento_array.pick_random())
+	pass # Replace with function body.
