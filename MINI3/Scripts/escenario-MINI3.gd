93a7df81229e684a3d3f66ac3d3fcacdd64f5406
Helena <115306258+HelenaMJ@users.noreply.github.com>
2025-05-26 13:28:04 +0200

    MINI3: Cuatro coches que se chocan

--- MINI3/Scripts/escenario-MINI3.gd
+++ MINI3/Scripts/escenario-MINI3.gd
@@ -21,10 +21,18 @@ func add_player(indice):
 	jugador.GIRODERECHA = "ui_right{n}".format({"n":indice+1})
 	jugador.AVANZAR = "ui_up{n}".format({"n":indice+1})
 	jugador.RETROCEDER = "ui_down{n}".format({"n":indice+1})
-	jugador.apply_scale(Vector2(0.5, 0.5))
+	jugador.apply_scale(Vector2(0.3, 0.3))
 	
-	if indice % 2 == 0:
+	if indice == 0:
+		$Label_jugador1.visible = true
 		jugador.rotate(deg_to_rad(180))
+	elif indice == 1:
+		$Label_jugador2.visible = true
+	elif indice == 2:
+		$Label_jugador3.visible = true
+		jugador.rotate(deg_to_rad(180))
+	elif indice == 3:
+		$Label_jugador4.visible = true
 	
 	add_child(jugador)
 
@@ -34,3 +42,33 @@ func _ready():
 	#for i in range(INICIO.num_jugadores):
 	for i in range(num_jugadores):
 		add_player(i)
+
+func _process(delta: float) -> void:
+	
+	for i in range(num_jugadores):
+		var jugador = jugadores[i]
+		
+		if jugador.nivel == 1:
+			jugador.puntos += delta * 1
+		elif jugador.nivel == 2:
+			jugador.puntos += delta * 5
+		elif jugador.nivel == 3:
+			jugador.puntos += delta * 10
+		
+		if i == 0:
+			$Label_jugador1.text = str(jugador.puntos)
+		elif i == 1:
+			$Label_jugador2.text = str(jugador.puntos)
+		elif i == 2:
+			$Label_jugador3.text = str(jugador.puntos)
+		elif i == 3:
+			$Label_jugador4.text = str(jugador.puntos)
+	
+
+func _on_puntos_body_entered(body: Node2D) -> void:
+	if body.get_class() == "CharacterBody2D":		#Si es un jugador
+		body.nivel += 1
+
+func _on_puntos_body_exited(body: Node2D) -> void:
+	if body.get_class() == "CharacterBody2D":		#Si es un jugador
+		body.nivel -= 1
--- MINI3/Scripts/jugador-MINI3.gd
+++ MINI3/Scripts/jugador-MINI3.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-const VELOCIDAD = 100
-const VELROT = 45
+const VELOCIDAD = 200
+const VELROT = 70
+const EMPUJE = 5
+const FRICCION = 50
 
 var GIROIZQUIERDA = "ui_left1"
 var GIRODERECHA = "ui_right1"
@@ -9,13 +11,33 @@ var AVANZAR = "ui_up1"
 var RETROCEDER = "ui_down1"
 
 var id : int = 1
+var nivel : int = 0
+var puntos : float = 0.0
+
+var empuje_acumulado : Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed(AVANZAR) or Input.is_action_pressed(RETROCEDER):
-		var diravance = - Input.get_axis(RETROCEDER, AVANZAR)
-		var direccion = Vector2(cos(rotation), sin(rotation))
-		move_and_collide(direccion * diravance * VELOCIDAD * delta)
-		
-		if Input.is_action_pressed(GIROIZQUIERDA) or Input.is_action_pressed(GIRODERECHA):
-			var dirgiro = Input.get_axis(GIROIZQUIERDA, GIRODERECHA)
-			rotate(deg_to_rad(dirgiro * delta * VELROT))
+	var diravance = - Input.get_axis(RETROCEDER, AVANZAR)
+	var dirgiro = Input.get_axis(GIROIZQUIERDA, GIRODERECHA)
+	
+	if diravance != 0:
+		rotate(deg_to_rad(dirgiro * delta * VELROT))
+	
+	var direccion = Vector2(cos(rotation), sin(rotation))
+	velocity = direccion * diravance * VELOCIDAD + empuje_acumulado
+	
+	move_and_slide()
+	empuje_acumulado = empuje_acumulado.move_toward(Vector2.ZERO, FRICCION * delta)
+	
+
+	for i in range(get_slide_collision_count()):
+		var choque = get_slide_collision(i)
+		var otroCoche = choque.get_collider()
+		if otroCoche is CharacterBody2D:
+			var empuje = (otroCoche.position - position).normalized() * EMPUJE
+			otroCoche.chocado(empuje)
+
+
+func chocado(empuje: Vector2) -> void:
+	empuje_acumulado += empuje
+			
