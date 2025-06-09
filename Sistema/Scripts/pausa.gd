bdd48bff2b8a4dc3c5e64376297469fb65044153
María del Mar <114167756+mamRobles@users.noreply.github.com>
2025-06-09 14:30:21 +0200

    añadido borde blanco al botón de pausa para poder distinguirlo en fondos oscuros y claros

--- Sistema/Scripts/pausa.gd
+++ Sistema/Scripts/pausa.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Boton_pausa.material.set("shader_parameter/thickness",10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
